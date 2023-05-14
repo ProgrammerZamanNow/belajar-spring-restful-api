b9367bbc3088e0d447b9f4b3b1e71a59a887ab9e
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-05-14 23:41:18 +0700

    User Entity

--- database.sql
+++ database.sql
@@ -1,3 +1,18 @@
 CREATE DATABASE belajar_spring_restful_api;
 
 USE belajar_spring_restful_api;
+
+CREATE TABLE users
+(
+    username         VARCHAR(100) NOT NULL,
+    password         VARCHAR(100) NOT NULL,
+    name             VARCHAR(100) NOT NULL,
+    token            VARCHAR(100),
+    token_expired_at BIGINT,
+    PRIMARY KEY (username),
+    UNIQUE (token)
+) ENGINE InnoDB;
+
+SELECT * FROM users;
+
+DESC users;
---
+++ src/main/java/programmerzamannow/restful/entity/User.java
@@ -0,0 +1,31 @@
+package programmerzamannow.restful.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    private String username;
+
+    private String password;
+
+    private String name;
+
+    private String token;
+
+    @Column(name = "token_expired_at")
+    private Long tokenExpiredAt;
+}

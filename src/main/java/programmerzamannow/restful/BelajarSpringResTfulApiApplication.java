1fe0b3226bad3710ebf9493c1cee58263a83d4a9
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-05-14 22:38:58 +0700

    belajar spring restful api

---
+++ src/main/java/programmerzamannow/restful/BelajarSpringResTfulApiApplication.java
@@ -0,0 +1,13 @@
+package programmerzamannow.restful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BelajarSpringResTfulApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BelajarSpringResTfulApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/programmerzamannow/restful/BelajarSpringResTfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package programmerzamannow.restful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BelajarSpringResTfulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
